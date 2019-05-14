c089b723cde376adeb7510c8fb2c126982c093a0
Dennis <dennis.basyrov@gmail.com>
2019-05-14 04:00:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,8 +10,8 @@ new_string << array[2]
 return new_string
 else 
   new_string1 = array[0...-1].join(", ")
-new_string << ", and "
-new_string << array[-1]
+new_string1 << ", and "
+new_string1 << array[-1]
 return new_string1
 end
 end
